dbdbc194bf810998498a1b19354ea50f55f314d3
SnailsNipp <SnailsNipp@users.noreply.github.com>
2020-03-31 17:41:45 +0300

    Upd test

--- market/Startup.cs
+++ market/Startup.cs
@@ -27,7 +27,7 @@ namespace market
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Die world!");
             });
         }
     }
